af1886ec091b385392f989f979de7dac43c26487
DrSmugleaf <javier.frayluis@gmail.com>
2016-04-02 11:26:48 +0200

    Basic mod structure
    Base mod files added

---
+++ modinfo.lua
@@ -0,0 +1,26 @@
+-- This information tells other players more about the mod
+name = "Mod Character Rebalancing 2"
+description = ""
+author = "DrSmugleaf"
+version = "1.0.0"
+
+priority = -100
+
+-- This is the URL name of the mod's thread on the forum; the part after the ? and before the first & in the url
+forumthread = ""
+
+-- This lets other players know if your mod is out of date, update it to match the current version in the game
+api_version = 10
+
+dst_compatible = true
+dont_starve_compatible = false
+reign_of_giants_compatible = true
+all_clients_require_mod = true
+
+icon_atlas = "modicon.xml"
+icon = "modicon.tex"
+
+server_filter_tags = {"mod character rebalancing 2","mcr","mcr2"}
+
+configuration_options =  {
+}
---
+++ modmain.lua
@@ -0,0 +1,8 @@
+MOD_NAME = "Mod Character Rebalancing 2"
+MOD_PREFIX = "MCR"
+MOD_ID = ""
+MOD_VERSION = "1.0.0"
+
+-- This library function allows us to use a file in a specified location.
+-- Allows use to call global environment variables without initializing them in our files.
+modimport("libs/env.lua")
