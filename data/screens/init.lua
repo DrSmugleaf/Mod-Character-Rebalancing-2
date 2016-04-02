ef666424cc60c8bd7f81e34b850d39fb57b2a1d6
DrSmugleaf <javier.frayluis@gmail.com>
2016-04-02 11:47:53 +0200

    Basic scripts
    Character specific and keystroke prevention scripts

---
+++ data/screens/init.lua
@@ -0,0 +1,5 @@
+-- Initialize equip action
+use "data/screens/chatinputscreen"
+
+-- Initialize givealltoplayer action.
+use "data/screens/consolescreen"
--- modmain.lua
+++ modmain.lua
@@ -6,3 +6,20 @@ MOD_VERSION = "1.0.0"
 -- This library function allows us to use a file in a specified location.
 -- Allows use to call global environment variables without initializing them in our files.
 modimport("libs/env.lua")
+
+-- Actions Initialization.
+use "data/actions/init"
+
+-- Component Initialization.
+use "data/components/init"
+
+-- Screens Initialization.
+use "data/screens/init"
+
+-- Widgets Initialization.
+use "data/widgets/controls"
+
+
+
+
+
