ddb4952d2d29cc6bc7034f8312b466f5648f0422
DrSmugleaf <javier.frayluis@gmail.com>
2016-04-03 14:52:03 +0200

    Added modifying stats with a leveling mechanic

--- data/scripts/modifycharacter.lua
+++ data/scripts/modifycharacter.lua
@@ -24,4 +24,57 @@ function ModifyCharacter:ModifyStats(inst, stats)
 	inst.components.hunger:SetRate(TUNING.WILSON_HUNGER_RATE * hungerrate or inst.components.hunger.hungerrate)
 end
 
+function ModifyCharacter:ModifyStatsWithLeveling(inst, stats)
+	local function applyupgrades(inst)
+		local max_upgrades = stats.max_upgrades or 15
+		inst.level = math.min(inst.level, max_upgrades)
+		
+		local health_percent = inst.components.health:GetPercent()
+		local hunger_percent = inst.components.hunger:GetPercent()
+		local sanity_percent = inst.components.sanity:GetPercent()
+		
+		inst.components.health.maxhealth = math.ceil(stats.initialhealth + inst.level * (stats.finalhealth - stats.initialhealth) / max_upgrades)
+		inst.components.hunger.max = math.ceil(stats.initialhunger + inst.level * (stats.finalhealth - stats.initialhealth) / max_upgrades)
+		inst.components.sanity.max = math.ceil(stats.initialsanity + inst.level * (stats.finalsanity - stats.initialsanity) / max_upgrades)
+		
+		inst.components.health:SetPercent(health_percent)
+		inst.components.hunger:SetPercent(hunger_percent)
+		inst.components.sanity:SetPercent(sanity_percent)
+	end
+	
+	local function oneat(inst, food)
+		for foodprefab, levelperfood in pairs(stats.levelingfood) do
+			if food and food.components.edible and food.prefab == foodprefab then
+				inst.level = inst.level + levelperfood
+				applyupgrades(inst)
+				inst.SoundEmitter:PlaySound("dontstarve/characters/wx78/levelup")
+			end
+		end
+	end
+	
+	local function onpreload(inst, data)
+		if data ~= nil and data.level ~= nil then
+			inst.level = data.level
+			applyupgrades(inst)
+			if data.health and data.health.health then inst.components.health:SetCurrentHealth(data.health.health) end
+			if data.hunger and data.hunger.hunger then inst.components.hunger.current = data.hunger.hunger end
+			if data.sanity and data.sanity.current then inst.components.sanity.current = data.sanity.current end
+			inst.components.health:DoDelta(0)
+			inst.components.hunger:DoDelta(0)
+			inst.components.sanity:DoDelta(0)
+		end
+	end
+	
+	local function onsave(inst, data)
+		data.level = inst.level
+	end
+	
+	inst.level = 0
+	inst.components.eater:SetOnEatFn(oneat)
+	applyupgrades(inst)
+	
+	inst.OnSave = onsave
+	inst.OnPreLoad = onpreload
+end
+
 return ModifyCharacter
