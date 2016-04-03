8b3744d304430edae825932b35fe4643f29c9fe6
DrSmugleaf <javier.frayluis@gmail.com>
2016-04-03 20:26:50 +0200

    Fixed crash when some stats are not specified

--- data/scripts/modifycharacter.lua
+++ data/scripts/modifycharacter.lua
@@ -12,8 +12,8 @@ function ModifyCharacter:ModifyStats(inst, stats)
 	inst.components.temperature.inherentinsulation = stats.winterinsulation or inst.components.temperature.inherentinsulation
 	inst.components.temperature.inherentsummerinsulation = stats.summerinsulation or inst.components.temperature.summerinsulation
 	
-	inst.components.locomotor.walkspeed = TUNING.WILSON_WALK_SPEED * stats.walkspeed or inst.components.locomotor.walkspeed
-	inst.components.locomotor.runspeed = TUNING.WILSON_RUN_SPEED * stats.runspeed or inst.components.locomotor.runspeed
+	inst.components.locomotor.walkspeed = stats.walkspeed and TUNING.WILSON_WALK_SPEED * stats.walkspeed or inst.components.locomotor.walkspeed
+	inst.components.locomotor.runspeed = stats.runspeed and TUNING.WILSON_RUN_SPEED * stats.runspeed or inst.components.locomotor.runspeed
 	
 	inst.components.sanity.dapperness = stats.dapperness or inst.components.sanity.dapperness
 	inst.components.sanity.dapperness_mult = stats.dapperness_mult or inst.components.sanity.dapperness_mult
@@ -21,7 +21,7 @@ function ModifyCharacter:ModifyStats(inst, stats)
 	inst.components.sanity.neg_aura_mult = stats.neg_aura_mult or inst.components.sanity.neg_aura_mult
 	
 	inst.components.eater.strongstomach = stats.strongstomach or inst.components.eater.strongstomach
-	inst.components.hunger:SetRate(TUNING.WILSON_HUNGER_RATE * hungerrate or inst.components.hunger.hungerrate)
+	inst.components.hunger:SetRate(stats.hungerrate and TUNING.WILSON_HUNGER_RATE * stats.hungerrate or inst.components.hunger.hungerrate)
 end
 
 function ModifyCharacter:ModifyStatsWithLeveling(inst, stats)
