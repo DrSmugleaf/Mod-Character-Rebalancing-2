f62062bbb95a545215226a079cc106d71de76933
DrSmugleaf <javier.frayluis@gmail.com>
2016-04-03 23:53:39 +0200

    Added tuning file

--- data/scripts/init.lua
+++ data/scripts/init.lua
@@ -1,3 +1,4 @@
 LogHelper = use "data/scripts/loghelper"
 ModifyCharacter = use "data/scripts/modifycharacter"
 RemoveEvent = use "data/scripts/removeevent"
+Tuning = use "data/scripts/tuning"
---
+++ data/scripts/tuning.lua
@@ -0,0 +1,42 @@
+TUNING = {}
+
+
+function Tune(overrides)
+	if overrides == nil then
+		overrides = {}
+	end
+	
+	local seg_time = 30
+	local total_day_time = seg_time*16
+	
+	local day_segs = 10
+	local dusk_segs = 4
+	local night_segs = 2
+	
+	--default day composition. changes in winter, etc
+	local day_time = seg_time * day_segs
+	local dusk_time = seg_time * dusk_segs
+	local night_time = seg_time * night_segs
+	
+	local multiplayer_attack_modifier = 1
+	local multiplayer_goldentool_modifier = 1
+	local multiplayer_armor_durability_modifier = 0.7
+	local multiplayer_armor_absorption_modifier = 1
+	local multiplayer_wildlife_respawn_modifier = 1
+
+	local wilson_attack = 34 * multiplayer_attack_modifier
+	local wilson_health = 150
+	local calories_per_day = 75
+	
+	local wilson_attack_period = .1
+	-----------------------
+	
+	local perish_warp = 1--/200
+	
+	TUNING =
+	{
+		
+    }
+end
+
+Tune()
