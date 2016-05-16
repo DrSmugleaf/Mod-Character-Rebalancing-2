2c7dc90e6983220ff25ee4e6208ab81f1672b863
DrSmugleaf <javier.frayluis@gmail.com>
2016-05-16 20:11:16 +0200

    Fixed recipe change and restructured modtuning.lua

--- data/scripts/modrecipe.lua
+++ data/scripts/modrecipe.lua
@@ -1,54 +1,28 @@
-ModRecipe = Class(function(self, name, ingredients, tab, level, placer, min_spacing, nounlock, numtogive, builder_tag, atlas, image)
-	self.name = name
-	
-	self.ingredients = {}
-	self.character_ingredients = {}
-	
-	for k,v in pairs(ingredients) do
-		if table.contains(CHARACTER_INGREDIENT, v.type) then
-			table.insert(self.character_ingredients, v)
-		else
-			table.insert(self.ingredients, v)
-		end
-	end
-	
-	self.product = name
-	self.tab = tab or AllRecipes[name].tab
-	
-	self.atlas = (atlas and resolvefilepath(atlas)) or resolvefilepath("images/inventoryimages.xml") or AllRecipes[name].atlas
-	self.image = image or (name .. ".tex") or AllRecipes[name].image
-	
-	self.sortkey = num or AllRecipes[name].sortkey
-	self.rpc_id = num or AllRecipes[name].rpc_id
-	self.level = level or AllRecipes[name].level or 0
-	self.level.ANCIENT = self.level.ANCIENT or AllRecipes[name].self.level.ANCIENT or 0
-	self.level.MAGIC = self.level.MAGIC or AllRecipes[name].self.level.MAGIC or 0
-	self.level.SCIENCE = self.level.SCIENCE or AllRecipes[name].self.level.SCIENCE or 0
-	self.level.SHADOW = self.level.SHADOW or AllRecipes[name].self.level.SHADOW or 0
-	self.placer = placer or AllRecipes[name].placer
-	self.min_spacing = min_spacing or AllRecipes[name].min_spacing or 3.2
-	
-	self.nounlock = nounlock or AllRecipes[name].nounlock or false
-	
-	self.numtogive = numtogive or AllRecipes[name].numtogive or 1
-	
-	self.builder_tag = builder_tag or nil
-	
-	num = num + 1
-	AllRecipes[name] = self
+ModRecipe = Class(function(self, inst)
+	self.inst = inst
 end)
 
-function ModRecipe:ChangeRecipe(...)
+function ModRecipe:ChangeRecipe(name, ingredients, tab, level, placer, min_spacing, nounlock, numtogive, builder_tag, atlas, image)
 	if not AllRecipes[name] then
-		return AddRecipe(...)
+		return AddRecipe(name, ingredients, tab, level, placer, min_spacing, nounlock, numtogive, builder_tag, atlas, image)
 	end
-	arg = {...}
-	print("ChangeRecipe", arg[1])
-	mod_protect_Recipe = false
-	local rec = Recipe(...)
-	mod_protect_Recipe = true
-	rec:SetModRPCID()
-	return rec
+	
+	AllRecipes[name].ingredients = ingredients or AllRecipes[name].ingredients
+	
+	AllRecipes[name].tab = tab or AllRecipes[name].tab
+	
+	AllRecipes[name].atlas = atlas or AllRecipes[name].atlas
+	AllRecipes[name].image = image or AllRecipes[name].image
+	
+	AllRecipes[name].level = level or AllRecipes[name].level
+	AllRecipes[name].placer = placer or AllRecipes[name].placer
+	AllRecipes[name].min_spacing = min_spacing or AllRecipes[name].min_spacing
+	
+	AllRecipes[name].nounlock = nounlock or AllRecipes[name].nounlock
+	AllRecipes[name].numtogive = numtogive or AllRecipes[name].numtogive
+	
+	AllRecipes[name].builder_tag = builder_tag or AllRecipes[name].builder_tag
+	
 end
 
 return ModRecipe
